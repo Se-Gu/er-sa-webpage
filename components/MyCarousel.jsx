f782b9098176aaed809894cfdbc46ca6c1b85e2e
Serhat Gürgenyatağı <63667747+Se-Gu@users.noreply.github.com>
2023-02-23 15:29:27 +0300

    carousel doesnt work as intended

--- components/MyCarousel.jsx
+++ components/MyCarousel.jsx
@@ -4,8 +4,8 @@ import { Carousel } from "react-responsive-carousel";
 
 export default function MyCarousel() {
   return (
-    <div>
-      <Carousel>
+    <div className="w-[100%] h-[50%]">
+      <Carousel showThumbs="false">
         <div>
           <img src="/1.jpg" alt="image1" />
           <p className="legend">Image 1</p>
@@ -17,6 +17,10 @@ export default function MyCarousel() {
         <div>
           <img src="/3.jpg" alt="image3" />
           <p className="legend">Image 3</p>
+        </div>
+        <div>
+          <img src="/4.jpg" alt="image3" />
+          <p className="legend">Image 4</p>
         </div>
       </Carousel>
     </div>

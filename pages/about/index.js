cce439453caf475696f9a0c2dfd949060b66b7fc
Doruk Onur Caliskan <63496036+dorukcaliskan02@users.noreply.github.com>
2023-02-26 12:55:59 +0300

    about yazısı sample

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return <div>Firmamız kuruluşundan bugüne anahtar teslim konut inşaatları, organize sanayi bölgelerinde fabrika binaları, idari binalar, sosyal tesisler, otel ve villa inşaatlarının projelendirme ve proje yönetim aşamalarında yer almakta, taahhütlerinde de etkin olarak görev yapmaktadır.
+
+  Firmamız imalat alanına girmeyen mal ve hizmet tedarikinde 1.sınıf çözüm ortakları ile çalışmaktadır. Bu politika sayesinde müşteri memnuniyetini en üst seviyeye çıkarmayı projelerinin odak noktasına koymuştur.
+  
+  Amacımız taahhüt ettiğimiz işleri; sözleşmelere uygun, zamanında ve öngörülen maliyetlerle sonuçlandırmaktır.
+  
+  Sevmeden yapılabilecek pek çok meslek vardır..Eğer böyle bir liste oluşturulmak istenseydi mimarlık bu listenin sonunda bile olmazdı!.
+  
+  Biz İşimizi Çok Seviyoruz…</div>;
 }
